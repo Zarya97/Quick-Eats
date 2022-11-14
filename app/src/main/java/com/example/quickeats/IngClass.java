d559798936143b7f00e02089eef4f552105a49d0
WigglyWorm73 <113563895+WigglyWorm73@users.noreply.github.com>
2022-11-14 13:05:05 -0600

    Update IngClass.java
    Added things to the ingredient class

--- app/src/main/java/com/example/quickeats/IngClass.java
+++ app/src/main/java/com/example/quickeats/IngClass.java
@@ -3,12 +3,26 @@ package com.example.quickeats;
 public class IngClass {
     private boolean checked;
     private String text;
+    private int ID;
 
-    public IngClass(String text) {
+    public IngClass(String text,int ID) {
         this.checked = false;
         this.text = text;
+        this.ID = ID;
     }
 
+    public void setText(String text) {
+		this.text = text;
+	}
+	
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+	
+	public int getID() {
+		return ID;
+	}
+    
     public boolean getCheck() {
         return checked;
     }
